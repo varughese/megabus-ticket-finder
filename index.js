553892d6908864cc4c52ad853e73df9d27eae3a8
Mat Varughese <matvarughese3@gmail.com>
2017-07-28 00:40:50 -0400

    email ticket functionality

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // Makes some route calls
+/*jshint esversion: 6 */
 
 var request = require("request");
 var fs = require("fs");
-var depature_date = "2017-10-13";
-
-var city_ids = {
-
-};
 
 var CITY = {
   PHL: 127,
@@ -27,7 +23,7 @@ var ENDPOINTS = {
     qs: {
       originId: CITY.PITT,
       destinationId: CITY.PHL,
-      departureDate: depature_date,
+      departureDate: "2017-10-13",
       totalPassengers: 1,
       concessionCount: 0,
       nusCount: 0,
@@ -36,8 +32,50 @@ var ENDPOINTS = {
   }
 };
 
-request(ENDPOINTS.JOURNEY_DETAILS)
-  .then(function(data) {
-    console.log(data);
-  })
-  // .pipe(fs.createWriteStream('journey-details.json'));
+// request(ENDPOINTS.JOURNEY_DETAILS)
+//   .pipe(fs.createWriteStream('journey-details.json'));
+
+
+var megabus = require("megabus");
+var nodemailer = require("nodemailer");
+
+var finder = new megabus.TicketFinder('9/1/2017', '9/30/2017', [
+  // New York <-> Philadelphia
+  new megabus.Route('New York', 'Philadelphia'),
+  new megabus.Route('Philadelphia', 'New York'),
+]);
+
+
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'megabus.ticket.finder@gmail.com',
+        pass: 'megabusticket'
+    }
+});
+
+// setup email data with unicode symbols
+var mailOptions = {
+    from: '"MEGA BUS FINDER" <megabustickerfinder@gmail.com>', // sender address
+    to: 'matvarughese3@gmail.com', // list of receivers
+    subject: 'Cheapest Ticket NY to PHL', // Subject line
+    html: '' // html body
+};
+
+finder.getTicketsInPriceRange(0, 5)
+  .then(function(tickets) {
+      tickets.forEach((ticket, idx) => {
+        mailOptions.html += `[${idx + 1}] ${ticket}<br>`;
+      });
+      console.log(`*** ${tickets.length} tickets found ***`);
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+      });
+
+  });
