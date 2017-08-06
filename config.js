6bb95710be4374337c3dcff5199b97996504a1b6
Mathew Varughese <mat.varughese3@gmail.com>
2017-08-06 13:52:59 -0400

    quick sercer

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: process.env.PORT || 8080,
+    database: 'mongodb://varughese:Blackjack21@ds011745.mlab.com:11745/hhnhs',
+    secret: 'jawnjawnjawnjawn'
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    path = require('path');
+
+var config = require('./config');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+	var TicketFinder = require("./lib/ticketfinder");
+	var Route = require("./lib/route");
+
+	var finder = new TicketFinder({ start: "TODAY", latestAvailable: true, weekends: true }, new Route("Pittsburgh", "Philadelphia").swap());
+
+	finder.getTicketsInPriceRange(0,5)
+		.then(function(tickets) {
+			res.send(JSON.stringify(tickets));
+		});
+
+});
+
+app.listen(config.port);
+console.log('Magic happening on port', config.port);
