6bec5c7c78d6a7a6b7ef0d049b145c8f37014813
Mathew Varughese <mat.varughese3@gmail.com>
2017-08-10 00:00:04 -0400

    lets test this email shit

--- index.js
+++ index.js
@@ -3,18 +3,40 @@
 var TicketFinder = require("./lib/ticketfinder");
 var Route = require("./lib/route");
 
-var finder = new TicketFinder({ start: "TODAY", latestAvailable: true, weekends: true }, new Route("Pittsburgh", "Philadelphia").swap());
+var finder = new TicketFinder({ start: "TODAY", latestAvailable: true, weekends: true }, new Route("Pittsburgh", "Philadelphia"));
 
 finder.getTicketsInPriceRange(0,5)
 	.then(function(tickets) {
-		tickets.map(n => console.log(n+""));
+		var email = tickets.map(n => n+"").join("<br><br>");
+		var nodemailer = require("nodemailer");
+		var transporter = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: 'megabus.ticket.finder@gmail.com',
+				pass: 'megabusticket'
+			}
+		});
+
+		var mailOptions = {
+			from: '"MEGA BUS FINDER" <megabustickerfinder@gmail.com>', // sender address
+			to: 'matvarughese3@gmail.com', // list of receivers
+			subject: 'Cheapest Ticket PHL to PITT', // Subject line
+			html: email // html body
+		};
+
+		transporter.sendMail(mailOptions, function(error, info) {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message %s sent: %s', info.messageId, info.response);
+		});
 	});
 
 // TODO: add option to search by days
 
 /*
 var megabus = require("megabus");
-var nodemailer = require("nodemailer");
+
 
 var finder = new megabus.TicketFinder('11/1/2017', '11/15/2017', [
 	// New York <-> Philadelphia
@@ -48,12 +70,7 @@ finder.getTicketsInPriceRange(0, 5)
 		console.log(`*** ${tickets.length} tickets found ***`);
 
 		// send mail with defined transport object
-		// transporter.sendMail(mailOptions, function(error, info) {
-		// 	if (error) {
-		// 		return console.log(error);
-		// 	}
-		// 	console.log('Message %s sent: %s', info.messageId, info.response);
-		// });
+
 
 	});
 */
