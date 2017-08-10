7f9822eb0dd756ac676fc4731714f617255c916d
Mathew Varughese <mat.varughese3@gmail.com>
2017-08-10 00:10:24 -0400

    lets check if my bootleg scheduler works

--- server.js
+++ server.js
@@ -29,10 +29,40 @@ app.get('/', function(req, res) {
 		.then(function(tickets) {
 			res.setHeader('content-type', 'text/plain');
 			var lines = tickets.map(n => n+"").join("\n\n\n");
+
+			var email = tickets.map(n => n+"").join("<br><br>");
+			var nodemailer = require("nodemailer");
+			var transporter = nodemailer.createTransport({
+				service: 'Gmail',
+				auth: {
+					user: 'megabus.ticket.finder@gmail.com',
+					pass: 'megabusticket'
+				}
+			});
+
+			var mailOptions = {
+				from: '"MEGA BUS FINDER" <megabustickerfinder@gmail.com>', // sender address
+				to: 'matvarughese3@gmail.com', // list of receivers
+				subject: 'Cheapest Ticket PHL to PITT', // Subject line
+				html: email // html body
+			};
+
+			transporter.sendMail(mailOptions, function(error, info) {
+				if (error) {
+					return console.log(error);
+				}
+				console.log('Message %s sent: %s', info.messageId, info.response);
+			});
+
 			res.send(lines);
 		});
 
 });
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://ride-king.herokuapp.com/");
+}, 86400000); // every 5 minutes (300000)
 
 app.listen(config.port);
 console.log('Magic happening on port', config.port);
