ff0e99dc63fa4bea9b40a4488ec93bb0d9d37fda
Mathew Varughese <mat.varughese3@gmail.com>
2017-08-17 01:55:21 -0400

    add mongo

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 /*jshint esversion: 6 */
 
+var mongo = require("mongodb").MongoClient,
+	url = 'mongodb://localhost:27017/megabustest';
+// Use connect method to connect to the Server
+
+
 var TicketFinder = require("./lib/ticketfinder");
 var Route = require("./lib/route");
 
-var finder = new TicketFinder({ start: "TODAY", latestAvailable: true, weekends: false },
-	[new Route("Pittsburgh", "Philadelphia"),
-	 new Route("Philadelphia", "Pittsburgh")]);
+var finder = new TicketFinder({
+	start: "TODAY",
+	latestAvailable: true,
+	weekends: false
+}, [new Route("Pittsburgh", "Philadelphia"),
+	new Route("Philadelphia", "Pittsburgh")
+]);
 
-finder.getTicketsInPriceRange(0,5)
+finder.getTicketsInPriceRange(0, 5)
 	.then(function(tickets) {
-		tickets.map(function(n) {
-			console.log(n+"");
+		tickets.forEach(function(ticket) {
+			console.log(ticket + "");
+			_saveTicket(ticket);
 		});
 	});
+
+
+function _saveTicket(ticket) {
+	MongoClient.connect(url, function(err, db) {
+		console.log(":::DB CONNECT:::");
+
+		// Insert a single document
+		db.collection('tickets').insertOne({
+			a: 1
+		}, function(err, r) {
+			assert.equal(null, err);
+			assert.equal(1, r.insertedCount);
+		});
+
+		db.collection('inserts').insertMany([{
+			a: 2
+		}, {
+			a: 3
+		}], function(err, r) {
+			assert.equal(null, err);
+			assert.equal(2, r.insertedCount);
+
+			db.close();
+		});
+	});
+}
+
+function MongoVersionTranscation(data, fields) {
+
+}
 
 // TODO: add option to search by days
 
 /*
+x = x.map(function(ticket) {
+return {price: ticket.price, origin: ticket.origin.cityCode, destination: ticket.destination.cityCode, date: ticket.date, departureTime: ticket.departureTime, arrivalTime: ticket.arrivalTime};
+}).map(function(ticket) {
+return {
+title: "$" +ticket.price + " [" + ticket.origin + "-->" + ticket.destination +"]",
+start: new Date(ticket.departureTime),
+end: new Date(ticket.arrivalTime)
+}
+})
+
 var megabus = require("megabus");
 
 
