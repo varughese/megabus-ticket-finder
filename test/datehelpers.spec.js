a58f1a1d25da74877cc055d714c5ca35e941aa47
Mathew Varughese <mat.varughese3@gmail.com>
2017-10-13 18:52:59 -0400

    added ability to choose specific days

--- test/datehelpers.spec.js
+++ test/datehelpers.spec.js
@@ -1,10 +1,35 @@
 let helpers = require("../lib/ticketbuilder/set-date-helpers");
 let assert = require("chai").assert;
-
+let moment = require("moment");
+let mocks = require("./mocks/datehelpers.mock.json");
 
 describe("Date Methods::", function() {
 
-	it("Formats Date::", function() {
-		assert(true);
+	describe("Get List of Dates::", function() {
+		it("Single day increment::", function() {
+			let startDate = moment(new Date("10/14/17")),
+				endDate = moment(new Date("10/28/17"));
+
+			let dateslist = helpers.getDatesList({}, startDate, endDate);
+
+			assert.deepEqual(dateslist, mocks.dayIncrement);
+		});
+
+		it("Weekends::", function() {
+			let startDate = moment(new Date("10/14/17")),
+				endDate = moment(new Date("11/28/17"));
+
+			let dateslist = helpers.getDatesList({ weekends: true }, startDate, endDate);
+			assert.deepEqual(dateslist, mocks.weekends);
+		});
+
+		it("Thursdays::", function() {
+			let startDate = moment(new Date("10/14/17")),
+				endDate = moment(new Date("11/28/17"));
+
+			let dateslist = helpers.getDatesList({ days: [4] }, startDate, endDate);
+			assert.deepEqual(dateslist, mocks.thursdays);
+		});
+
 	});
 });
