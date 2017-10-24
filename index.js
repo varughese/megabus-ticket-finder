0c2b1a9a57b5e65c1e2783b5c83d3037c86e3a12
Mathew Varughese <mat.varughese3@gmail.com>
2017-10-23 22:59:32 -0400

    change ticket

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const Route = require("./lib").Route;
 
 var finder = new TicketFinder({
 	start: "TODAY",
-	// latestAvailable: true,
-	end: "2017-12-20",
+	latestAvailable: true,
+	// end: "2017-12-20",
 	// start: "2017-11-17",
 
 	// weekends: true,
 	days: [4, 5, 6, 0, 1]
 }, [new Route("Pittsburgh", "PHILADELPHIA"),
-	// new Route("PSU", "Pittsburgh")
+	new Route("Philadelphia", "Pittsburgh")
 ]);
 
-finder.getTicketsInPriceRange(0, 1)
+finder.getTicketsInPriceRange(0, 10)
 	.then(function(payload) {
 		var originId = payload.tickets[0].origin.cityId;
 		payload.tickets.forEach(function(ticket) {
