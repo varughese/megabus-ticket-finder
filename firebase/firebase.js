d38543798b599ffe2ad1064397cd6507cf5be882
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-14 00:03:41 -0500

    remove secret key lol

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const config = require("../lib/config");
 
-const serviceAccount = require("./secretkey.json");
+const serviceAccount = require("./secretkey.js");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
