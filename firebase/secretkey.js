60a0a23a58b77f33feab6d35fc3bc7f0ce7eb5dd
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-16 19:05:54 -0500

    changing the env variables to be FIREBASE for readability

--- firebase/secretkey.js
+++ firebase/secretkey.js
@@ -1,9 +1,9 @@
 module.exports = {
   "type": "service_account",
   "project_id": "megabus-ticket-tracker",
-  "private_key_id": process.env.FB_PRIVATE_KEY_ID,
-  "private_key": process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  "client_email": process.env.FB_CLIENT_EMAIL,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": "112567519630224098537",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://accounts.google.com/o/oauth2/token",
