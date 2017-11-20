0571fe6ce7a56f74ab5153704d573d0045db15ed
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-20 09:38:30 -0500

    got a really simple web app going

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     morgan = require('morgan'),
     path = require('path');
 
-var config = require('./config');
+var config = require('./lib/config');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,49 +20,34 @@ app.use(function(req, res, next) {
 app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
-	var TicketFinder = require("./lib/ticketfinder");
-	var Route = require("./lib/route");
+	const TicketFinder = require("./lib").TicketFinder;
+	const Route = require("./lib").Route;
 
-	var finder = new TicketFinder({ start: "TODAY", latestAvailable: true, weekends: true }, new Route("Pittsburgh", "Philadelphia").swap());
+	var finder = new TicketFinder({
+		start: "TODAY",
+		latestAvailable: true,
+		// end: "2017-12-20",
+		// start: "2017-11-17",
+
+		weekends: true,
+		// days: [6]
+	}, [new Route("Pittsburgh", "PSU"), new Route("PSU", "Pittsburgh"),
+	new Route("Philadelphia", "Pittsburgh").swap()
+	]);
 
 	finder.getTicketsInPriceRange(0,5)
-		.then(function(tickets) {
-			res.setHeader('content-type', 'text/plain');
-			var lines = tickets.map(n => n+"").join("\n\n\n");
-
-			var email = tickets.map(n => n+"").join("<br><br>");
-			var nodemailer = require("nodemailer");
-			var transporter = nodemailer.createTransport({
-				service: 'Gmail',
-				auth: {
-					user: 'megabus.ticket.finder@gmail.com',
-					pass: 'megabusticket'
-				}
+		.then(function(payload) {
+			res.setHeader('content-type', 'text/html');
+			var lines = "";
+			payload.tickets.forEach(function(ticket) {
+				lines+= ticket.toHtml();
 			});
 
-			var mailOptions = {
-				from: '"MEGA BUS FINDER" <megabustickerfinder@gmail.com>', // sender address
-				to: 'matvarughese3@gmail.com', // list of receivers
-				subject: 'Cheapest Ticket PHL to PITT', // Subject line
-				html: email // html body
-			};
-
-			transporter.sendMail(mailOptions, function(error, info) {
-				if (error) {
-					return console.log(error);
-				}
-				console.log('Message %s sent: %s', info.messageId, info.response);
-			});
 
 			res.send(lines);
 		});
 
 });
 
-var http = require("http");
-setInterval(function() {
-    http.get("https://ride-king.herokuapp.com/");
-}, 86400000); // every 5 minutes (300000)
-
-app.listen(config.port);
-console.log('Magic happening on port', config.port);
+app.listen(config.PORT);
+console.log('Magic happening on port', config.PORT);
