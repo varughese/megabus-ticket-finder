619194f6dacc684eb151a2b08a768dbe20b4633a
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-20 23:42:11 -0500

    timeout to stop the firebase from failing

--- test/firebase.spec.js
+++ test/firebase.spec.js
@@ -5,7 +5,7 @@ var assert = require("chai").assert;
 
 
 describe("Firebase::", function() {
-	this.timeout(200);
+	this.timeout(2000);
 	let closeFirebase = require("../firebase/go-offline");
 
 	beforeEach(function() {
