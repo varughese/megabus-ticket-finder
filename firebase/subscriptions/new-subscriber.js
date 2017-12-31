752e97fda2aff6c24340871d85658aa4b7cddbdb
Mathew Varughese <mat.varughese3@gmail.com>
2017-12-31 12:00:32 -0500

    new subscriber module

--- firebase/subscriptions/new-subscriber.js
+++ firebase/subscriptions/new-subscriber.js
@@ -1,8 +1,21 @@
 let firebase = require("./firebase");
+let validateFilterId = require("../../lib/helpers/get-filterId");
 let db = firebase.database();
 
 let subscriptionsRef = db.ref("subscriptions");
 
+/*
+{
+	filterId: MMDDYY_ORG_DST
+	email: __@__.com
+	price: ##
+}
+*/
+
 module.exports = function(data) {
-	let filterId = data.filterId;
+	let filterId = validateFilterId(data.filterId);
+	subscriptionsRef.child(`${filterId}/subscriptions`).push({
+		email: data.email,
+		price: data.price
+	});
 };
