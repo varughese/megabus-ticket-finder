773b52ce5a49edb3c2d96696a174a9b08334dec4
Mathew Varughese <mat.varughese3@gmail.com>
2018-01-02 00:04:10 -0500

    start date can be a js Date

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const Route = require("./lib").Route;
 const CONFIG = require("./lib").config;
 
 var finder = new TicketFinder({
-	start: "2018-02-10",
-	// latestAvailable: true,
-	end: "2018-02-10",
+	start: "TODAY",
+	latestAvailable: true,
+	// end: "2018-02-10",
 	// start: "2017-11-17",
 
 	// weekends: true,
 	// days: [0, 1]
 	// days: [4, 5]
-	days: [0, 1, 4, 5]
+	days: [0]
 }, [
-	new Route("Dallas", "Houston"),
+	// new Route("Dallas", "Houston"),
 	// new Route("PSU", "Pittsburgh").swap(),
-//	new Route("Pittsburgh", "Philly"),
-//	new Route("Pittsburgh", "Philly").swap(),
+	// new Route("Pittsburgh", "Philly"),
+	new Route("Pittsburgh", "Philly"),
 ]);
 
 let saveTicket = require("./firebase/save-ticket");
