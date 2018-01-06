c9fb2ed3599f60e3f2538b8f492dcd9c5d4fc431
Mathew Varughese <mat.varughese3@gmail.com>
2018-01-05 21:22:08 -0500

    email system works

---
+++ email-test.js
@@ -0,0 +1,30 @@
+const moment = require("moment");
+
+const TicketFinder = require("./lib").TicketFinder;
+const Route = require("./lib").Route;
+
+
+let endDate = moment().add(20, 'days').format("YYYY-MM-DD"),
+	startDate = moment().add(10, 'days').format("YYYY-MM-DD");
+
+let finder = new TicketFinder({ start: startDate, end: endDate },
+	[new Route("Pittsburgh", "Philadelphia")]);
+
+
+function getTickets() {
+	console.log("GETTING TICKETS");
+	return finder.findTickets()
+		.then(function(payload) {
+			console.log("\n\nGOT TICKETS");
+			return payload[0];
+		});
+}
+
+const emailer = require("./lib/emailer/emailer");
+
+
+getTickets()
+	.then(function(ticket) {
+		console.log("EMAILING");
+		emailer("varhawk5@gmail.com", "single", ticket);
+	});
