d78cb4b899a7bbc8a89a9e0710e3e4d20bfbdd08
Mathew Varughese <mat.varughese3@gmail.com>
2018-12-17 23:18:18 -0500

    i am dumb and accidentally change the gcloud credentials

--- firebase/secretkey.js
+++ firebase/secretkey.js
@@ -1,12 +1,12 @@
 module.exports = {
 	'type': 'service_account',
-	'project_id': 'megabus-ticket-tracker',
+	'project_id': 'megabus-ticket-finder',
 	'private_key_id': process.env.FIREBASE_PRIVATE_KEY_ID,
 	'private_key': process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	'client_email': process.env.FIREBASE_CLIENT_EMAIL,
-	'client_id': '112567519630224098537',
-	'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-	'token_uri': 'https://accounts.google.com/o/oauth2/token',
-	'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-	'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lq8nn%40megabus-ticket-tracker.iam.gserviceaccount.com'
+	"client_id": "107335115715245893666",
+	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+	"token_uri": "https://oauth2.googleapis.com/token",
+	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+	"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mw7sp%40megabus-ticket-finder.iam.gserviceaccount.com"
 };
